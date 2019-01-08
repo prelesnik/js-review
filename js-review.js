c153151be17a650b8efd56364c2f3f6ddc979574
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-01-08 12:26:27 -0500

    added a function that completes number 2 of the homework

--- js-review.js
+++ js-review.js
@@ -2,5 +2,38 @@
 // 1/8/19
 // js-review.js
 
+
+//function calls
+fizzBuzz();
+
+
 //number 1
 console.log("Introduction to JavaScript");
+
+//number 2
+function fizzBuzz()
+{
+    for (let i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 != 0)
+        {
+            console.log("Fizz");
+        }
+
+        else if (i % 5 == 0 && i % 3 != 0)
+        {
+            console.log("Buzz");
+        }
+
+        else if (i % 5 == 0 && i % 3 == 0)
+        {
+            console.log("FizzBuzz");
+        }
+
+        else
+        {
+            console.log(i);
+        }
+    }
+}
+
